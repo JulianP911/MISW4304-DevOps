596681a45e634bdeb3618c7b1f384abc37b4bacd
Laura Cabra <lh.cabra@uniandes.edu.co>
2024-10-12 17:54:29 -0500

    add basic auth

--- Blacklists/src/blueprints/operations.py
+++ Blacklists/src/blueprints/operations.py
@@ -1,17 +1,34 @@
 from flask import jsonify, request, Blueprint, Response
 from models.model import Blacklist, BlacklistJsonSchema, db
+from dotenv import load_dotenv
 
+load_dotenv()
+
+import os
 operations_blueprint = Blueprint('operations', __name__)
+
+# Token predefinido
+AUTH_TOKEN = os.environ['TOKEN']
+
+# Función para verificar el token de autorización
+def verify_token():
+    token = request.headers.get('Authorization')
+    if token != AUTH_TOKEN:
+        return False
+    return True
 
 @operations_blueprint.route('/blacklists', methods=['POST'])
 def blacklist():
+    if not verify_token():
+        return jsonify({"error": "Unauthorized"}), 401
+
     data = request.json
     email = data.get('email')
     app_uuid = data.get('app_uuid')
     blocked_reason = data.get('blocked_reason')
 
     if not email or not app_uuid or not blocked_reason:
-        Response(status=400)
+        return Response(status=400)
 
     try:
         new_post = Blacklist(email=email, app_uuid=app_uuid, blocked_reason=blocked_reason)
@@ -23,4 +40,6 @@ def blacklist():
 
 @operations_blueprint.route('/blacklists/ping', methods=['GET'])
 def health_check():
+    if not verify_token():
+        return jsonify({"error": "Unauthorized"}), 401
     return Response('pong', status=200)
