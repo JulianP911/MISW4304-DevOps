d839c64898a46c977ab1e9983bd41734f6112454
Julian Padilla Molina <julianpadilla@tyba.com.co>
2024-10-14 23:32:30 -0500

    Se ajusta orden de los archivos y naming para beanstalk

--- application.py
+++ application.py
@@ -13,26 +13,26 @@ else:
     from blueprints.operations import operations_blueprint
     from errors.errors import ApiError
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = (
+application.config["SQLALCHEMY_DATABASE_URI"] = (
     f"postgresql://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{os.environ['DB_NAME']}"
 )
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+application.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-app_context = app.app_context()
+app_context = application.app_context()
 app_context.push()
 
-db.init_app(app)
+db.init_app(application)
 db.create_all()
 
-app.register_blueprint(operations_blueprint)
+application.register_blueprint(operations_blueprint)
 
 
-@app.errorhandler(ApiError)
+@application.errorhandler(ApiError)
 def handle_exception(err):
     return "", err.code
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    application.run(host="0.0.0.0", port=3000)
